cdd98975adcdcd573410303efaa4804ce4c0206c
Circe-melki <midory798464@gmail.com>
2025-11-27 19:54:17 +0100

    make a temporary call history page

---
+++ lib/pages/home/call_history_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:contact/widgets/call_history_card.dart';
+
+class CallHistoryPage extends StatelessWidget {
+  const CallHistoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('call history')
+    );
+  }
+}
