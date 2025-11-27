06188db06641ad9b8501b8e322587d8f2c7faa35
Circe-melki <midory798464@gmail.com>
2025-11-27 19:54:40 +0100

    add the three contact pages to the home page

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,23 +1,56 @@
 import 'package:flutter/material.dart';
+import 'contacts_page.dart';
+import 'favorites_page.dart';
+import 'call_history_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = const [
+    ContactsPage(),
+    FavoritesPage(),
+    CallHistoryPage(),
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home Page"),
+        title: const Text("Contacts App"),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          "Welcome! You are logged in.",
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.w500,
+      body: _pages[_selectedIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onTabTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contacts),
+            label: "Contacts",
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: "Favorites",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: "History",
+          ),
+        ],
       ),
     );
   }
