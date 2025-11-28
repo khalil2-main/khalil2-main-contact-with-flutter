854101d779c89c02fded364888ecf3a5db8a844d
Circe-melki <midory798464@gmail.com>
2025-11-28 09:39:55 +0100

    add a logout popup butto,n and a searchbar

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-import 'contacts_page.dart';
-import 'favorites_page.dart';
-import 'call_history_page.dart';
-import 'keypad_page.dart';   // <-- Make sure to import your KeypadPage
+import 'package:provider/provider.dart';
+import 'package:contact/services/auth_service.dart';
+import 'package:contact/pages/auth/login_page.dart';
+
+import 'package:contact/pages/Contact/contacts_page.dart';
+import 'package:contact/pages/Contact/favorites_page.dart';
+import 'package:contact/pages/Contact/call_history_page.dart';
+
+import 'keypad_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,6 +19,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
+  final TextEditingController _searchController = TextEditingController();
+  //navigation routes
   final List<Widget> _pages = const [
     ContactsPage(),
     FavoritesPage(),
@@ -25,13 +32,65 @@ class _HomePageState extends State<HomePage> {
       _selectedIndex = index;
     });
   }
+  //logout function call
+  Future<void> _logout(BuildContext context) async {
+    final authService = context.read<AuthService>();
+    await authService.logout();
+
+    if (!mounted) return;
+
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (_) => const LoginPage()),
+          (route) => false,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Contacts App"),
-        centerTitle: true,
+        // search bar (still not functional)
+        title: SizedBox(
+          height: 40,
+          child: TextField(
+            controller: _searchController,
+            decoration: InputDecoration(
+              hintText: 'Search...',
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: const EdgeInsets.symmetric(horizontal: 12),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50),
+                borderSide: BorderSide.none,
+              ),
+              prefixIcon: const Icon(Icons.search),
+            ),
+          ),
+        ),
+
+        actions: [
+          //use poppup menu to logout
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              if (value == "logout") {
+                _logout(context);
+              }
+            },
+            itemBuilder: (context) => [
+              const PopupMenuItem(
+                value: "logout",
+                child: Row(
+                  children: [
+                    Icon(Icons.logout, color: Colors.red),
+                    SizedBox(width: 10),
+                    Text("Logout"),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
 
       body: _pages[_selectedIndex],
@@ -43,9 +102,9 @@ class _HomePageState extends State<HomePage> {
             MaterialPageRoute(builder: (context) => const KeypadPage()),
           );
         },
-        child: const Icon(Icons.dialpad),   // keypad icon
+        child: const Icon(Icons.dialpad),
       ),
-
+      //navigation buttons
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onTabTapped,
